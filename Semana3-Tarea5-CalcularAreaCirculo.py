203d710c76fc090c61a90c54651f942578a0abbf
JohnVindel <jev30_vindel@hotmail.com>
2021-07-04 14:08:18 -0600

    primer commit

---
+++ Semana3-Tarea5-CalcularAreaCirculo.py
@@ -0,0 +1,8 @@
+#*****Ejercicio 3 - Calsular el area de un Circulo*****#
+
+valor1 = int(input("Ingrese el primer valor: "))
+valor2 = int(input("Ingrese el segundo valor: "))
+
+calculo = (3.1416) * (valor1 * valor2)
+
+print("El radio del circulo es ", calculo)
